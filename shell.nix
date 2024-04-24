5481b28ba8ce89c2a6fa71baed30badacb3a05db
Henri Peurasaari <henri.peurasaari@helsinki.fi>
2024-04-24 19:36:02 +0300

    Reduce copypaste in nix files

--- shell.nix
+++ shell.nix
@@ -1,10 +1,5 @@
 let
-  pkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/6143fc5eeb9c4f00163267708e26191d1e918932.tar.gz";
-    sha256 = "sha256:0rmbq6jr21fvcspxshaiwmm5a1z66pykjm2f5imww9nhxn6dfgzv";
-  }) {};
-  compilerVersion = "ghc98";
-  compiler = pkgs.haskell.packages."${compilerVersion}";
+  inherit (import nix/nixpkgs.nix) pkgs compiler;
 in
   compiler.developPackage {
     root = ./.;
