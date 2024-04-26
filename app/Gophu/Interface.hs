366aa8dc251dd8bc01e2aa07d4424f0fba2a390a
Henri Peurasaari <henri.peurasaari@helsinki.fi>
2024-04-26 15:14:57 +0300

    Radically simplify Gophu.Interface
    I was trying to do too many things at once with Gophu.Interface while
    not even supporting half of the things I had included. The extra bloat
    has been removed and may or may not be revisited later.

--- app/Gophu/Interface.hs
+++ app/Gophu/Interface.hs
@@ -1,30 +1,11 @@
 module Gophu.Interface (handleArgs) where
 
-data Arg = Flag String | URI String deriving Show
-
-isURI :: Arg -> Bool
-isURI (Flag _) = False
-isURI (URI _)  = True
+import Gophu.Network (ResourceName)
+import Network.Simple.TCP (HostName)
 
 -- It's not pretty, but it works. Don't nerdsnipe yourself, not yet.
 -- There will always be time for refactoring later.
-handleArgs :: [String] -> [Arg]
-handleArgs = either error id . validateArgs . parseArgs
-
-parseArgs :: [String] -> [Arg]
-parseArgs = map argparser
-  where
-    argparser :: String -> Arg
-    argparser "" = error "empty arg?"
-    argparser arg@(c:s)
-      -- This flag parsing is obviously flawed, but I'll revisit it when I
-      -- begin to work with flags proper.
-      -- Use `turtle`?
-      | c == '-' = Flag s
-      | otherwise = URI arg
-
-validateArgs :: [Arg] -> Either String [Arg]
-validateArgs args
-  | length (filter isURI args) == 1 = Right args
-  | any isURI args                  = Left "Too many URIs provided"
-  | otherwise                       = Left "No URI was provided"
+handleArgs :: [String] -> Either String (HostName, ResourceName)
+handleArgs [arg] = Right $ break (=='/') arg
+handleArgs _ = Left $ "argument error: gophu takes a single argument, "
+                <> "which is the address URI"
