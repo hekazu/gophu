d6cfb861f57976f2aae07783ee888c3b7e0914ca
Henri Peurasaari <henri.peurasaari@helsinki.fi>
2024-04-29 15:42:57 +0300

    Print argument error to stderr

--- app/Gophu/Interface.hs
+++ app/Gophu/Interface.hs
@@ -2,8 +2,10 @@ module Gophu.Interface (runGophuWithArgs) where
 
 import Gophu.Network
 
+import qualified System.IO as IO
+
 runGophuWithArgs :: [String] -> IO ()
-runGophuWithArgs = either putStrLn performGopherQuery . handleArgs
+runGophuWithArgs = either (IO.hPutStr IO.stderr) performGopherQuery . handleArgs
 
 -- It's not pretty, but it works. Don't nerdsnipe yourself, not yet.
 -- There will always be time for refactoring later.
